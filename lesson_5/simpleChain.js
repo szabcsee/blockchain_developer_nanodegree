8f806258fff141e5c432ed119f5a7670c305319a
Szabolcs Bokonyi <szabcsee@gmail.com>
2021-06-24 11:22:28 +0200

    project folder renamed

--- lesson_5/simpleChain.js
+++ lesson_5/simpleChain.js
@@ -5,7 +5,6 @@ class Block {
         this.hash = "";
         this.height = 0;
         this.body = data;
-        this.time = 0;
         this.previousBlockHash = "";
     }
 }
@@ -13,13 +12,21 @@ class Block {
 class Blockchain {
     constructor() {
         this.chain = [];
-        this.addBlock(new Block("First block in the chain."));
+        this.addBlock(this.createGenesisBlock());
+    }
+    getLatestBlock(){
+        return this.chain[this.chain.length -1];
     }
     addBlock(newBlock) {
+        newBlock.height = this.chain.length;
+        newBlock.timeStamp = new Date().getTime().toString().slice(0,-3);
         if (this.chain.length > 0) {
-            newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash
+            newBlock.previousBlockHash = this.getLatestBlock().hash
         }
         newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
         this.chain.push(newBlock);
+    }
+    createGenesisBlock(){
+        return new Block("First block in the chain - Genesis block");
     }
 }
---
+++ lesson_6/notes.md
@@ -0,0 +1,19 @@
+## Digital assets
+
+## Encoding decoding
+
+## Proof of existence
+Poex.io
+the digital asset is not stored on the blockchain but its content is in this way it does not reveal the private content but it can be verified.
+
+## Securing Assets Using a Digital Identity
+
+## Web Service with Blockchain Considerations
+    - consumers require a user interface
+
+## Planning Web Services with Our Private Blockchain
+
+## RESTful APIs with Node.js Framework
+
+## Blockchain generations
+    - Differences between blockchain 1.0 and 2.0 blockchains

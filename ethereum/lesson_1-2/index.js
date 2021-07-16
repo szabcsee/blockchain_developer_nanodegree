e3a2004e9ec3d9f61e3571aa0bb4f14f44b76281
Szabolcs Bokonyi <szabcsee@gmail.com>
2021-07-16 15:02:49 +0200

    Working dApp with Ganache

--- ethereum/lesson_1-2/index.js
+++ ethereum/lesson_1-2/index.js
@@ -3,15 +3,15 @@ const Web3 = require('web3'),
     url = 'http://127.0.0.1:8545', //Ganache url
     web3 = new Web3(url); 
 
-let sendingAddress = '0xF483d4c0C8b5A9CB450D345De25E761BCA39Bbbd',
-    receivingAddress = '0x249cefB9165De32D6f9D67f2A3Ae2cf972f6a9C7',
-    privateKey = Buffer.from('490727426d01fb0a171491012631563e4dd863a49f3f300118587bbc5f468967','hex',),
+let sendingAddress = '0x942D31Ce813E1B34917B97031B628227d00b13d0',
+    receivingAddress = '0x55763dED5BCE10f8886861BE54d418Cef6EDEc17',
+    privateKey = Buffer.from('ebdcb2ea2542057cfe4a3d3808504ecf9be1c16bbbbb5513cf7d83fb69e87586','hex',),
     txParams = {
-        nonce: ascii_2_0xhex(0),
+        nonce: ascii_2_0xhex(7),
         to: receivingAddress,
         gasPrice: ascii_2_0xhex(20000000),
         gasLimit: ascii_2_0xhex(30000),
-        value: ascii_2_0xhex(100),
+        value: ascii_2_0xhex(10000000000000000000),
         data: "0x00"
     }
 const tx = new EthereumTx(txParams, { chain: 'mainnet', hardfork: 'petersburg' });
